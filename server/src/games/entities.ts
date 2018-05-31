de2b941921040ea7e8d74dbbebc5565387d45322
Claudia Groot <cl@udia.me>
2018-06-01 01:10:00 +0200

    nicer to look at and tests

--- server/src/games/entities.ts
+++ server/src/games/entities.ts
@@ -7,7 +7,7 @@ export type Board = [ Row, Row, Row, Row, Row, Row, Row ]
 
 type Status = 'pending' | 'started' | 'finished'
 
-const emptyRow: Row = [null, null, null, null, null, null]
+export const emptyRow: Row = [null, null, null, null, null, null]
 const emptyBoard: Board = [ emptyRow, emptyRow, emptyRow, emptyRow, emptyRow, emptyRow, emptyRow ]
 
 @Entity()
